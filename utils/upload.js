7d6ab2e35b4e62933d88eae10f6d5e5dd22579ab
shiva167 <108976426+shiva167@users.noreply.github.com>
2024-01-29 17:01:07 +0530

    multer configs

--- utils/upload.js
+++ utils/upload.js
@@ -1,8 +1,8 @@
-// import multer from 'multer'
+import multer from 'multer'
 // const upload = multer({dest:'uploads'});
 // export default upload;
 
-const multer = require('multer');
+// const multer = require('multer');
 const storage = multer.diskStorage({
   destination: '/tmp/uploads', // Use /tmp or another writable directory
   filename: (req, file, cb) => {
